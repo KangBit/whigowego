09b557d1b9dffca42b80aaa4a87be47d567d563b
kangbit <kangbit@smartscore.kr>
2022-11-30 20:23:01 +0900

    주류 테이블 추가

--- src/views/DrinkView.vue
+++ src/views/DrinkView.vue
@@ -1,4 +1,69 @@
 <template>
   <h1>주류</h1>
+  <table>
+    <thead>
+      <tr>
+        <td>이름</td>
+        <td>용량(ML)</td>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in list" :key="item.id">
+        <td>{{ item.name }}</td>
+        <td>{{ item.capacity }}ML</td>
+      </tr>
+    </tbody>
+  </table>
   <button class="btn btn-primary btn-more">More</button>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import sampleData from "../sampleData";
+
+export default defineComponent({
+  name: "DrinkView",
+  components: {},
+  props: {},
+  data() {
+    return {
+      list: sampleData,
+    };
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+table {
+  width: 80vw;
+  margin-inline: auto;
+  border-collapse: collapse;
+  margin-bottom: 1rem;
+}
+
+thead {
+  font-size: 0.75rem;
+  font-weight: 600;
+  letter-spacing: 1px;
+}
+
+tbody {
+  font-size: 0.875rem;
+  font-weight: 400;
+}
+
+tr {
+  border-bottom: 1px solid #ecebf1;
+}
+
+td {
+  &:nth-of-type(1) {
+    width: 70%;
+  }
+  &:nth-of-type(2) {
+    width: 30%;
+  }
+  text-align: left;
+  padding: 0.75rem 1.5rem;
+}
+</style>
